273a2dfd8a32311cb9b886a54a9f6b17b3281d06
Siddhartha Mishra <siddharthamishra@Siddharthas-MacBook-Air.local>
2024-04-22 00:25:48 +0530

    updated backend1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://websocket5-fr.onrender.com",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
